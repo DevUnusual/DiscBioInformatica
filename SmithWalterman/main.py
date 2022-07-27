05466b108c277314d76bd512ab2e204057daa438
carlos meneses <carlos.meneses.g.s@gmail.com>
2022-07-27 01:23:49 -0300

    criando matriz_auxiliar para definir caminho

--- SmithWalterman/main.py
+++ SmithWalterman/main.py
@@ -13,36 +13,42 @@ file.close()
 
 data = re.findall("([A-Za-z]+)", data)
 
-matriz = np.zeros((len(data[0])+1, len(data[1])+1), dtype=list)
+matriz = np.empty((len(data[0])+1, len(data[1])+1), dtype=int)
+matriz_auxiliar = np.empty((len(data[0])+1, len(data[1])+1), dtype=str)
 
 for i in range(1,len(data[0])+1):
     matriz[i][0] = gap * i
     if i <= len(data[1]):
         matriz[0][i] = gap * i
 
+caminho = []
 
 for i in range(1,len(data[0])+1):
     for j in range(1,len(data[1])+1):
         caminho = '' ##d-diagonal, v-vertical, h-horizontal
         if data[0][i-1] == data[1][j-1]:
-            maior_resultado = max(matriz[i-1][j-1][0] + match, matriz[i-1][j][0] + gap, matriz[i][j-1][0] + gap)
+            maior_resultado = max(matriz[i-1][j] + gap, matriz[i][j-1] + gap, matriz[i-1][j-1] + match)
             if maior_resultado == matriz[i-1][j] + gap:
                 caminho += 'v'
             if maior_resultado == matriz[i][j-1] + gap:
-                caminho += '-h'
+                caminho += 'h'
             if maior_resultado == matriz[i-1][j-1] + match:
-                caminho += '-d'
-            
-            matriz[i][j] = [maior_resultado, caminho]
+                caminho += 'd'
+            matriz[i][j] = maior_resultado
+            matriz_auxiliar[i][j] = caminho
+
         else:
-            maior_resultado = max(matriz[i-1][j][0] + gap, matriz[i][j-1][0] + gap, matriz[i-1][j-1][0] + mismatch)
+            maior_resultado = max(matriz[i-1][j] + gap, matriz[i][j-1] + gap, matriz[i-1][j-1] + mismatch)
             if maior_resultado == matriz[i-1][j] + gap:
                 caminho += 'v'
             if maior_resultado == matriz[i][j-1] + gap:
-                caminho += '-h'
+                caminho += 'h'
             if maior_resultado == matriz[i-1][j-1] + mismatch:
-                caminho += '-d'
-            matriz[i][j] = [maior_resultado, caminho]
+                caminho += 'd'
+            matriz[i][j] = maior_resultado
+            matriz_auxiliar[i][j] = caminho
+        
+        print(f"{maior_resultado} : {matriz[i-1][j] + gap} / {matriz[i][j-1] + gap} / {matriz[i-1][j-1] + mismatch} / {matriz[i-1][j-1] + match} - {caminho}")
 
 
 
@@ -52,11 +58,10 @@ for i in range(1,len(data[0])+1):
 
 
 
-def save_matriz(matriz, caminho):
+def save_matriz(matriz):
     file = open("output.txt", "w")
-    np.savetxt(file, matriz, fmt="%+4d",delimiter="|")
+    np.savetxt(file, matriz, fmt="%+4s",delimiter="|")
     print(matriz)
-    np.savetxt(file, caminho)
 
-
-save_matriz(matriz, caminho)
+print(matriz_auxiliar)
+save_matriz(matriz_auxiliar)
