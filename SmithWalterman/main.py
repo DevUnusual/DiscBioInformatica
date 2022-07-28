eb76403b3b7cb3f9136c6b7932322432ae45e9a6
carlos meneses <carlos.meneses.g.s@gmail.com>
2022-07-28 00:35:59 -0300

    aparentemente finalizado o algoritmo, falta só ajustes de esteticas e compreensao do codigo, comentar e afins

--- SmithWalterman/main.py
+++ SmithWalterman/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import re
 ## gap mismatch match
-gap = -2
+gap = -3
 mismatch = -1
 match = 3
 
@@ -13,15 +13,14 @@ file.close()
 
 data = re.findall("([A-Za-z]+)", data)
 
-matriz = np.empty((len(data[0])+1, len(data[1])+1), dtype=int)
-matriz_auxiliar = np.empty((len(data[0])+1, len(data[1])+1), dtype=str)
+matriz = np.empty((len(data[0])+1, len(data[1])+1), dtype=int) #seq1 na linha e seq2 na coluna
+matriz_auxiliar = np.empty((len(data[0])+1, len(data[1])+1), dtype=(str, 4))
 
+matriz[0][0] = 0
 for i in range(1,len(data[0])+1):
     matriz[i][0] = gap * i
     if i <= len(data[1]):
         matriz[0][i] = gap * i
-
-caminho = []
 
 for i in range(1,len(data[0])+1):
     for j in range(1,len(data[1])+1):
@@ -48,20 +47,64 @@ for i in range(1,len(data[0])+1):
             matriz[i][j] = maior_resultado
             matriz_auxiliar[i][j] = caminho
         
-        print(f"{maior_resultado} : {matriz[i-1][j] + gap} / {matriz[i][j-1] + gap} / {matriz[i-1][j-1] + mismatch} / {matriz[i-1][j-1] + match} - {caminho}")
+        #print(f"{maior_resultado} : {matriz[i-1][j] + gap} / {matriz[i][j-1] + gap} / {matriz[i-1][j-1] + mismatch} / {matriz[i-1][j-1] + match} - {caminho}")
+
+backtrace = [[],[]]
+topo = [len(data[0]), len(data[1])]
+
+while matriz[topo[0]][topo[1]] != 0:
+    y_seq = topo[0]
+    x_seq = topo[1]
+    if y_seq == 0 or x_seq == 0:
+        if y_seq == 0:
+            backtrace[0].append("-")
+            backtrace[1].append(data[1][x_seq-1])
+            topo[1] -= 1
+        elif x_seq == 0:
+            backtrace[0].append(data[0][y_seq-1])
+            backtrace[1].append("-")
+            topo[0] -= 1
+        
+    elif 'd' in matriz_auxiliar[y_seq][x_seq]:
+        backtrace[0].append(data[0][y_seq-1])
+        backtrace[1].append(data[1][x_seq-1])
+        topo[0] -= 1
+        topo[1] -= 1
+    elif 'v' in matriz_auxiliar[y_seq][x_seq]:
+        backtrace[0].append(data[0][y_seq-1])
+        backtrace[1].append("-")
+        topo[0] -= 1
+    elif 'h' in matriz_auxiliar[y_seq][x_seq]:
+        backtrace[0].append("-")
+        backtrace[1].append(data[1][x_seq-1])
+        topo[1] -= 1
+
+
+##print(backtrace[0])
+#print(backtrace[1])
+
+resultado = np.full((2, len(backtrace[0])),fill_value=(backtrace[0], backtrace[1]),dtype=str)
+print(resultado)
 
 
 
 
 
-
-
-
-
-def save_matriz(matriz):
+def save_matriz(resultado):
     file = open("output.txt", "w")
-    np.savetxt(file, matriz, fmt="%+4s",delimiter="|")
-    print(matriz)
+    #np.savetxt(file, resultado, fmt="%s")
+    for i in resultado[0][::-1]:
+        file.write(i)
+    file.write("\n")
+    for i in resultado[1][::-1]:
+        file.write(i)
+    file.write("\n")
+    file.close()
 
-print(matriz_auxiliar)
-save_matriz(matriz_auxiliar)
+#print(matriz_auxiliar)
+save_matriz(resultado)
+
+file = open("output.txt", "a")
+np.savetxt(file, matriz[::-1], fmt="%+4s",delimiter="|")
+file.write("----------------------------------------------------------------------\n")
+np.savetxt(file, matriz_auxiliar[::-1], fmt="%+4s",delimiter="|")
