24d3b7092c04cf3715421032222b712a49be1263
carlos meneses <carlos.meneses.g.s@gmail.com>
2022-09-27 09:41:03 -0300

    algoritmo de remontagem finalizado mas nao otimizado

--- Remontagem/atividade.py
+++ Remontagem/atividade.py
@@ -9,21 +9,26 @@ print(kmer)
 
 # separar em tuplas, juntar iguais fazer o caminho
 
+
 def separarTuplas(kmer):
     contador = 0
     tuplas = []
     for i in sorted(kmer):
-        tuplas.append((i[:len(i)-1], i[1:len(i)], contador)) # kmer-1, -1kmer, posicao tabela
+        # kmer-1, -1kmer, posicao tabela
+        tuplas.append((i[:len(i)-1], i[1:len(i)], contador))
         contador += 1
-    #print(tuplas, 'tuplas')
+    # print(tuplas, 'tuplas')
     return np.array(tuplas)
 
-def makeTable(tuplas, kmers, nKmers): #####CONTAR TODOS OS KMERS E FAZER UMA TABELA COM TAMANHO TOTAL DELES SEM REPETIDOS NO MOMENTO TEM NOMES REPETIDOS NA TABELA
-    tabela = np.zeros((nKmers,nKmers),dtype=int)
+
+# CONTAR TODOS OS KMERS E FAZER UMA TABELA COM TAMANHO TOTAL DELES SEM REPETIDOS NO MOMENTO TEM NOMES REPETIDOS NA TABELA
+def makeTable(tuplas, kmers, nKmers):
+    tabela = np.zeros((nKmers, nKmers), dtype=int)
     for kmer1 in tuplas:
         tabela[int(kmers.index(kmer1[0]))][int(kmers.index(kmer1[1]))] += 1
     print(tabela)
     return tabela
+
 
 def lenkmers(tuplas):
     kmers = 0
@@ -35,9 +40,10 @@ def lenkmers(tuplas):
         if i[1] not in visitados:
             kmers += 1
             visitados.append(i[1])
-    #print('kmers', kmers)
-    print('visitados', visitados) # AT TC TG TT CA CC GA AC GC 
+    # print('kmers', kmers)
+    # print('visitados', visitados)  # AT TC TG TT CA CC GA AC GC
     return (visitados, kmers)
+
 
 def percorreTabela(tabela, kmers):
     linha = 0
@@ -46,35 +52,59 @@ def percorreTabela(tabela, kmers):
         kmerProximo.append([])
 
     for i in tabela:
-        #print('i',i, 'linha', linha, 'pos', i.nonzero()[0])
-        if not i.any(where= 1):
-            print(f'--> {kmers[linha]} final do caminho')
+        # print('i',i, 'linha', linha, 'pos', i.nonzero()[0])
+        if not i.any(where=1):
+            # print(f'--> {kmers[linha]} final do caminho')
             final = linha
-        print(f'lista {kmerProximo} len {len(kmerProximo)} acessando 0 {kmerProximo[0]}')
+        # print(
+            # f'lista {kmerProximo} len {len(kmerProximo)} acessando 0 {kmerProximo[0]}')
         for k in i.nonzero()[0]:
-            print(f'{kmers[linha]} apontando para {kmers[k]}')
-            value = tabela [linha][k]
+            # print(f'{kmers[linha]} apontando para {kmers[k]}')
+            value = tabela[linha][k]
             for j in range(value):
-                kmerProximo[int(kmers.index(kmers[k]))].append(linha)
+                kmerProximo[int(kmers.index(kmers[linha]))].append(k)
         linha = linha + 1
+    # print(kmerProximo, 'kmerProximo')
     return (final, kmerProximo)
+
 
 def caminho(final, ligacoes, visitados):
     passoApasso = []
-    while ligacoes != []:
-        passoApasso.append(final)
-        if len(ligacoes[final]) > 1:
-            final = ligacoes[final].pop()
-        final = ligacoes[final]
-        ligacoes.remove(final)
-        ligacoes = ligacoes.insert()
+    passoApasso.append(final)
+    while np.array(ligacoes, dtype=object).size > 0:
+        for liglist in ligacoes:
+            for unidade in liglist:
+                if unidade == final:
+                    kmerAnterior = ligacoes.index(liglist)
+                    ligacoes[kmerAnterior].remove(unidade)
+                    passoApasso.append(kmerAnterior)
+                    final = kmerAnterior
+                    # print(f'passo a passo {passoApasso}')
+                    # print(f'ligacoes {ligacoes}')
+                    continue
+                # print(f'{unidade} <- unidade, final -> {final}')
+
+    # print(passoApasso, 'passo a passo')
+    return passoApasso
+
+
+def imprimirCaminho(caminho, kmers):
+    resultado = ""
+    resultado += kmers[caminho[0]]
+    for i in caminho[1:]:
+        resultado += kmers[i][-1]
+
+    return resultado
+
 
 tuplas = separarTuplas(kmer)
 (visitados, kmers) = lenkmers(tuplas)
 tabela = makeTable(tuplas, visitados, kmers)
 (final, ligacoes) = percorreTabela(tabela, visitados)
-way = caminho( final, ligacoes, visitados)
-
-print('ligacoes', ligacoes)
+way = caminho(final, ligacoes, visitados)
+resultadoFinal = imprimirCaminho(way[:: -1], visitados)
+#print('ligacoes', ligacoes)
 print('way', way)
+print('resultadoFinal', resultadoFinal, len(resultadoFinal))
 
+kmerExport(resultadoFinal)
--- Remontagem/importExport.py
+++ Remontagem/importExport.py
@@ -1,10 +1,11 @@
-def kmerImport(location = "kmer.txt"):
+def kmerImport(location="Atividade_9.COMPUTACAO.PROVA.Vale 10.composicao_1_Bioinformaticas_size_30000_k_50.txt"):
     kmerfile = open(location, "r")
     kmer = kmerfile.read()
     kmer = kmer.split(',')
     verify(len(kmer[0]), kmer)
     print("Kmers importados com sucesso")
     return kmersOrdenados(kmer)
+
 
 def verify(tam, kmer):
     if kmer[-1] == '':
@@ -14,10 +15,12 @@ def verify(tam, kmer):
             print("Error: sequencia de kmers invalida")
             exit()
 
+
 def kmersOrdenados(kmer):
     kmer.sort()
     return kmer
 
+
 def kmerExport(way):
-    kmerfile = open("kmer.txt", "w")
+    kmerfile = open("output.txt", "w")
     kmerfile.write(way)
